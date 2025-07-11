dcaaff268786a4a368718c67c8a12b94a8c828cb
webtesting22 <148945321+Sachin-Prepseed@users.noreply.github.com>
2025-07-11 10:47:19 +0530

    Updated pages

---
+++ src/Components/Products/Produts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Produts = () => {
+    return (
+        <div>Produts</div>
+    )
+}
+
+export default Produts
---
+++ src/Components/Promotion/Promotion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Promotion.css"
+const Promotion = () => {
+  return (
+    <div>Promotion</div>
+  )
+}
+
+export default Promotion
